e15ad0822652107c0415a6eefed3816eafb0ed7d
Gus Flusser <gus.flusser@gmail.com>
2026-02-03 13:32:47 -0800

    fix: env

--- bin/duck-hunt.ts
+++ bin/duck-hunt.ts
@@ -1,15 +1,18 @@
 #!/opt/homebrew/opt/node/bin/node
 import * as cdk from "aws-cdk-lib";
+import "dotenv/config";
 import { DuckHuntStack } from "../lib/duck-hunt-stack";
 
 export interface DuckHuntStackProps extends cdk.StackProps {
   uniqueId: string;
 }
 
-const uniqueId = process.env.UNIQUE_ID || "prod-1";
+if (!process.env.UNIQUE_ID) {
+  throw Error("UNIQUE_ID must be set in the environment");
+}
 
 const app = new cdk.App();
 
-new DuckHuntStack(app, `DuckHuntStack-${uniqueId}`, {
-  uniqueId,
+new DuckHuntStack(app, `DuckHuntStack-${process.env.UNIQUE_ID}`, {
+  uniqueId: process.env.UNIQUE_ID,
 } as DuckHuntStackProps);
